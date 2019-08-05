93051c4eea8286f91bd7bc248898e61af3dceaf1
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-05 17:44:42 +0100

    Creates home page with the rules of the game

--- controller.rb
+++ controller.rb
@@ -4,6 +4,10 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/game.rb')
 also_reload('./models*')
 
+get '/' do
+  erb(:home)
+end
+
 get '/:hand1/:hand2' do
 
   @result = Game.play(params[:hand1], params[:hand2])
